88175f5637c9746f06a043eb76be6da0ac5f4983
mfrance <mf212mf@gmail.com>
2015-10-01 22:21:53 +0900

    create code

---
+++ week-4/calculate-grade/my_solution.rb
@@ -0,0 +1,34 @@
+# Calculate a Grade
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def get_grade(percentage)
+
+    if percentage  >= 90
+     return "A"
+
+    elsif percentage >= 80 && percentage <90
+      return "B"
+
+     elsif percentage >= 70 && percentage <80
+      return "C"
+
+     elsif percentage >= 60 && percentage <70
+      return "D"
+
+     else percentage <= 50
+      return "F"
+  end
+
+end
+
+
+
+
+
+
+
+
